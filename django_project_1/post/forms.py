c10fa62a0c325dc16c51a8ae1b9ef8f4e34584dd
Dinosy <asn6878@gmail.com>
2023-05-28 03:32:28 +0900

    forms.py attrs 추가

--- django_project_1/post/forms.py
+++ django_project_1/post/forms.py
@@ -1,7 +1,14 @@
 from django import forms
 from .models import *
+from django.forms import TextInput
 
 class PostCreateForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ("author", "title", "body", 'category')
+        widgets = {
+            'author': TextInput(attrs={
+                'class' : "form-control",
+                'placeholder' : "제목을 작성하세요",
+            }),
+        }
